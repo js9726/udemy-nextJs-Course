af8fe05c8755e37b25e6dffc7f01dbd9240e05a8
js9726 <jiesheng2697@gmail.com>
2025-04-16 16:19:59 +0800

    feat: add fetch posts functionality and update post handling

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 import Modal from './Modal';
 
+
 function PostList( {isPosting,onStopPosting} ) {
+    // fetch('http://localhost:8080/posts').then(response=>response.json()).then(data=>{
+    //     setPosts(data.posts);
+    // })
     const[posts,setPosts] = useState([]);
 
+    useEffect(()=>{
+        async function fetchPosts(){
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+        fetchPosts();
+       },[]) 
+       //This prevent infinite loop, using useEffect hook
+       //useEffect hook runs after every render cycle, but we can add a second argument which is a list of dependencies
+       //if we add an empty list, it will run only once, if we add a list of dependencies, it will run only when the dependencies chang
+    
+        
     function addPostHandler(postData){
+        fetch('http://localhost:8080/posts',{
+            method:'POST',
+            body:JSON.stringify(postData),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
         setPosts((prevPosts)=>[postData,...prevPosts]);
     }
     return(
@@ -19,7 +43,7 @@ function PostList( {isPosting,onStopPosting} ) {
             </Modal>
             ) : false}
             <ul id="" className= {classes.posts}>
-                {posts.map((post) => <Post author={post.author} body={post.body} />)}
+                {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
 
             </ul>
         </>
